0340233bd65c6cd81fb97413ef70b41ede0b800d
Isaac Rosemberg <isaacrosemberg@gmail.com>
2025-06-09 10:47:46 -0700

    Delete 2. Spidering_and_Modeling_Email_Data_PY4E directory

--- 2. Spidering_and_Modeling_Email_Data_PY4E/gline.js
+++
@@ -1,4 +0,0 @@
-gline = [ ['Year','umich.edu','uct.ac.za','columbia.edu','mac.com','indiana.edu','berkeley.edu','unl.edu','mtu.edu','ucdavis.edu','ufp.pt'],
-['2005-12',57,14,13,12,12,12,12,11,11,10],
-['2006-01',0,0,0,0,0,0,0,0,0,1]
-];
